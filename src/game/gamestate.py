407ab7babbfcf8e02070ffb2f355fa8edd7a5c32
scshafe <scshafe@umich.edu>
2025-04-04 13:35:53 -0500

    changed function for detecting a win to return an array with indicating the winning moves

--- src/game/gamestate.py
+++ src/game/gamestate.py
@@ -14,24 +14,29 @@ class Tile(Enum):
 
 
 def board_contains_3_in_a_row_for_piece(board, piece):
+    win = [[False for i in range(3)] for j in range(3)]
     for row in range(3):
         if board[row][0] == board[row][1] and board[row][1] == board[row][2] and board[row][0] == piece:
-            return True
+            win[row][0] = win[row][1] = win[row][2] = True
+            return win
     for col in range(3):
         if board[0][col] == board[1][col] and board[1][col] == board[2][col] and board[0][col] == piece:
-            return True
+            win[0][col] = win[1][col] = win[2][col] = True
+            return win
     
     # diagonals
     if board[0][0] == board[1][1] and board [1][1] == board[2][2] and board[1][1] == piece:
-        return True
+        win[0][0] = win[1][1] = win[2][2] = True
+        return win
     if board[2][0] == board[1][1] and board [1][1] == board[0][2] and board[1][1] == piece:
-        return True
-    return False
+        win[2][0] = win[1][1] = win[0][2] = True
+        return win
+    return []
 
 
 
 def board_contains_3_in_a_row(board):
-    return board_contains_3_in_a_row_for_piece(board, Tile.P1) or board_contains_3_in_a_row_for_piece(board, Tile.P2)
+    return board_contains_3_in_a_row_for_piece(board, Tile.P1) + board_contains_3_in_a_row_for_piece(board, Tile.P2)
 
 
 
