e04e9a292ab8200f89b9eea6ebc6adcc899e83e3
scshafe <scshafe@umich.edu>
2025-04-04 13:13:37 -0500

    fixed interface screen options

--- src/interfaces/ncursesterminal/input_validators.py
+++ src/interfaces/ncursesterminal/input_validators.py
@@ -5,7 +5,7 @@ menu_screen_options = {"p", "c", "i", "q"}
 
 run_player_turn_options = {curses.KEY_UP, curses.KEY_RIGHT, curses.KEY_DOWN, curses.KEY_LEFT, 10, "m"} 
 
-interface_screen_options = {1 , 2, 3, 4}
+interface_screen_options = {"1" , "2", "3", "4"}
 
 select_change_name_screen_options = {"1", "2"}
 
